e520535ab8e16a818779be5207869bba8a66319a
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-18 18:47:47 +0530

    Install react-navigation which is similar to reactjs-router.

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>LETS BUILD SIGNAL CLONE</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>LETS BUILD SIGNAL CLONE</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
