e75229de43ecf587f0c8cc030c1c6039e392e750
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-18 18:40:26 +0530

    firebase configuration added.

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAZUY4d-SASIRb5JFwFrLXJsleQW3D4v0",
+    authDomain: "signal-clone-acd1a.firebaseapp.com",
+    projectId: "signal-clone-acd1a",
+    storageBucket: "signal-clone-acd1a.appspot.com",
+    messagingSenderId: "571354643826",
+    appId: "1:571354643826:web:65ba4af8e5c3ffa37c71eb"
+};
