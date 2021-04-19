1b131e23052d28b012b52205810fda1eb68507e0
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-19 16:59:55 +0530

    imported react-native-elements with button, input, image

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Button, Input, Image } from 'react-native-elements';
+import { StatusBar } from 'expo-status-bar'
 
 const LoginScreen = () => {
     return (
         <View>
-            <Text>I am the login screen</Text>
+            <StatusBar style="light" />
+            <Image source={{
+                uri: "https://blog.mozilla.org/internetcitizen/files/2018/08/signal-logo.png"
+            }} />
         </View>
     )
 }
