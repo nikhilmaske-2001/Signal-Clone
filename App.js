21ccb8b8bd3703581537deb0b56be75fc9f9229b
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-19 16:41:55 +0530

    Login Screen: Design a login screen header.

--- App.js
+++ App.js
@@ -3,15 +3,25 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import LoginScreen from './screens/LoginScreen';
+import { createStackNavigator } from '@react-navigation/stack'
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "white" },
+  headerTitleAlign: 'center',
+  headerTintColor: "white",
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <Text>LETS BUILD SIGNAL CLONE</Text>
-        <StatusBar style="auto" />
-      </View>
-    </NavigationContainer>
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer >
   );
 }
 
